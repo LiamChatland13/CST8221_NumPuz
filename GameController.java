c2064f46529ff1cc62de510fac2248a357cdfc66
c4lfbrah <101197780+c4lfbrah@users.noreply.github.com>
2022-11-03 15:31:20 -0400

    New classes for MVC Implementation

---
+++ GameController.java
@@ -0,0 +1,46 @@
+package game;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.GridLayout;
+import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+@SuppressWarnings({ "unused", "serial" })
+
+public class GameController extends GameView{
+	GameController(GameModel model, GameView view){
+		//This needs to be in the controller I think
+        dim.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String s = (String) dim.getSelectedItem();
+                switch (s) {
+                case "3":
+                	Grid.removeAll();
+                	Grid.revalidate();
+                	Grid.repaint();
+                	c.add(createGrid(3,3));;
+                	break;
+
+               case "4":
+               		Grid.removeAll();
+            	    Grid.revalidate();
+               	    Grid.repaint();
+            	    c.add(createGrid(4,4));
+                   	break;
+                   
+               case "5":
+               		Grid.removeAll();
+               		Grid.revalidate();
+               		Grid.repaint();
+            	    c.add(createGrid(5,5));
+                    break;
+                }
+            }
+        });
+	}
+}
