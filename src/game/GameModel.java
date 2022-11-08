612c38da95863b06c40171f1478f9977f2f01301
c4lfbrah <101197780+c4lfbrah@users.noreply.github.com>
2022-11-07 21:23:44 -0500

    test

---
+++ src/game/GameModel.java
@@ -0,0 +1,19 @@
+package game;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.GridLayout;
+import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+@SuppressWarnings( { "unused", "serial" })
+
+
+public class GameModel extends GameView {
+	//Declaring Grid dimensions & Grid
+	public static final int HEIGHT = 800; 
+	public static final int WIDTH = 650;
+}
